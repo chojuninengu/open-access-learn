9b87cb2a8be1308857bcfb3cecfa6d9576bb1807
chojuninengu <chojuninengu@gmail.com>
2025-04-09 10:39:01 +0100

    feat: add API configuration for Cameroon Education and YouTube services

---
+++ frontend/src/config/api.js
@@ -0,0 +1,17 @@
+export const API_CONFIG = {
+    CAMEROON_EDUCATION: {
+        BASE_URL: 'https://api.cameroon-education.gov.cm',
+        ENDPOINTS: {
+            CURRICULUM: '/curriculum',
+            SUBJECTS: '/subjects',
+            TOPICS: '/topics',
+            PAST_QUESTIONS: '/past-questions'
+        }
+    },
+    YOUTUBE: {
+        BASE_URL: 'https://www.googleapis.com/youtube/v3',
+        ENDPOINTS: {
+            SEARCH: '/search'
+        }
+    }
+}; 
