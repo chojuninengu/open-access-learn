e57db8cf391fabd61914b07f2eafab2ed8857e52
chojuninengu <chojuninengu@gmail.com>
2025-04-09 12:33:44 +0100

    feat: integrate I18nextProvider for multilingual support and include AITutor component in the main application layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 import './App.css';
 
 // Pages
@@ -16,29 +17,31 @@ import Settings from './pages/Settings';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import LanguageSelector from './components/LanguageSelector';
+import AITutor from './components/AITutor';
 
 function App() {
-  const { t } = useTranslation();
-
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
-        <LanguageSelector />
-        <main className="flex-grow container mx-auto px-4 py-8">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/curriculum" element={<Curriculum />} />
-            <Route path="/past-questions" element={<PastQuestions />} />
-            <Route path="/settings" element={<Settings />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <I18nextProvider i18n={i18n}>
+      <Router basename={process.env.PUBLIC_URL}>
+        <div className="min-h-screen flex flex-col">
+          <Navbar />
+          <LanguageSelector />
+          <main className="flex-grow container mx-auto px-4 py-8">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/curriculum" element={<Curriculum />} />
+              <Route path="/past-questions" element={<PastQuestions />} />
+              <Route path="/settings" element={<Settings />} />
+            </Routes>
+          </main>
+          <Footer />
+          <AITutor />
+        </div>
+      </Router>
+    </I18nextProvider>
   );
 }
 
